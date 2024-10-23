dd2da99c78a04fda2b6fa77cb9e473e17ad36156
derek0k <derek0kim@gmail.com>
2024-10-23 09:55:20 +0900

    refac: Header 네비게이션 로직 간소화

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,60 +2,42 @@
 
 import Link from "next/link";
 import AlertIcon from "../ui/icons/AlertIcon";
-import AlertFillIcon from "../ui/icons/AlertFillIcon";
 import SearchIcon from "../ui/icons/SearchIcon";
-import SearchFillIcon from "../ui/icons/SearchFillIcon";
-import { usePathname } from "next/navigation";
 import CustomButton from "../ui/CustomButton";
+import { useRouter } from "next/navigation";
 
-const menus = [
-  {
-    href: "/search",
-    icon: <SearchIcon />,
-    clicckedIcon: <SearchFillIcon />,
-  },
-  {
-    href: "/alert",
-    icon: <AlertIcon />,
-    clicckedIcon: <AlertFillIcon />,
-  },
-];
+const MENU_ICONS = [<SearchIcon key="search" />, <AlertIcon key="alert" />];
 
 export default function Header() {
-  const pathName = usePathname();
-  const session = false;
+  const router = useRouter();
+  const isLoggedIn = false;
+
+  const handleLogin = () => {
+    router.replace("/login");
+    // TODO: 로그인 처리 로직 추가
+  };
+
+  const handleLogout = () => {
+    // TODO: 로그아웃 처리 로직 추가
+  };
 
   return (
     <header className="flex justify-between items-center px-4 bg-white border-b">
       <Link href="/">
-        <h1 className="text-3xl font-bold">Smit</h1>
+        <h1 className="text-3xl font-bold cursor-pointer">Smit</h1>
       </Link>
       <nav>
         <ul className="flex items-center gap-4 py-4">
-          {menus.map(({ href, icon, clicckedIcon }) => (
-            <li key={href}>
-              {
-                <Link href={href}>
-                  {pathName === href ? clicckedIcon : icon}
-                </Link>
-              }
+          {MENU_ICONS.map((icon) => (
+            <li key={icon.key} className="cursor-pointer">
+              {icon}
             </li>
           ))}
           <li>
-            {session ? (
-              <CustomButton
-                text="로그아웃"
-                onClick={() => {
-                  // signout
-                }}
-              />
+            {isLoggedIn ? (
+              <CustomButton text="로그아웃" onClick={handleLogout} />
             ) : (
-              <CustomButton
-                text="로그인"
-                onClick={() => {
-                  // signIn
-                }}
-              />
+              <CustomButton text="로그인" onClick={handleLogin} />
             )}
           </li>
         </ul>
