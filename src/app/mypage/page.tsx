3f1a890c25d50372c04a453fca78296492fe4ad1
derek0k <derek0kim@gmail.com>
2024-10-23 11:22:40 +0900

    refac: Header 네비게이션 로직 간소화

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -8,7 +8,7 @@ import UserProfile from "./components/UserProfile";
 
 export default function MyPage() {
   return (
-    <div className="w-screen h-screen mt-10">
+    <div className="flex flex-col mt-10">
       <UserProfile />
       <hr className="my-10 border-2" />
       <MyPostList />
