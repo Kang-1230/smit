cbd48cc00015ce97519fda7cdc7760c3846ac0db
hsw <hongsw6934@gmail.com>
2024-10-23 16:45:26 +0900

    feat : 메인화면 '+' 버튼 클릭 시 Dropdown 및 pagenation

--- src/app/write/components/Dropdown.tsx
+++ src/app/write/components/Dropdown.tsx
@@ -1,27 +1,28 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
-
 export default function Dropdown() {
   return (
     <Menu>
-      <MenuButton>My account</MenuButton>
-      <MenuItems anchor="bottom">
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/settings">
-            Settings
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/support">
-            Support
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/license">
-            License
-          </a>
-        </MenuItem>
-      </MenuItems>
+      <div className="fixed bottom-4 right-4">
+        <MenuButton className="text-xl font-bold border-zinc-950 border rounded-full ... w-12 h-12 flex items-center justify-center">
+          +
+        </MenuButton>
+        <MenuItems anchor="top">
+          <MenuItem>
+            <a className="block data-[focus]:bg-blue-100 right-4" href="/write">
+              스터디 그룹 생성
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              className="block data-[focus]:bg-blue-100 right-4"
+              href="/write?type=study"
+            >
+              스터디 모집글 작성
+            </a>
+          </MenuItem>
+        </MenuItems>
+      </div>
     </Menu>
   );
 }
---
+++ src/app/write/study/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>스터디 생성 페이지입니다.</div>
+  )
+}
+
+export default page
