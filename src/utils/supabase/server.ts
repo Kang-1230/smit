276928e148a03934143eb9a312f6c30ef393cba8
doo1b <tngml928@gmail.com>
2024-10-24 17:13:35 +0900

    rename: server-actions 에서 supabase-server로 변경

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,57 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
-export async function login(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
-}
-
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
-}
-
-export async function signout() {
-  const supabase = createClient();
-
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.log("signout error");
-    redirect("/error");
-  }
-}
 
 export const createClient = () => {
   const cookieStore = cookies();
--- src/utils/supabase/supabase-server.ts
+++ src/utils/supabase/supabase-server.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { Tables } from "../../../database.types";
 import { createClient } from "./server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const getUser = async () => {
   const supabase = createClient();
@@ -15,4 +18,72 @@ export const getUser = async () => {
   }
 
   return user;
+};
+
+//로그인 함수
+export async function login(formData: FormData) {
+  const supabase = createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
+
+//회원가입 함수
+export async function signup(formData: FormData) {
+  const supabase = createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
+
+//로그아웃 함수
+export async function signout() {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.log("signout error");
+    redirect("/error");
+  }
+}
+
+export const fetchUserPosts = async (id: string) => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("post")
+    .select("*")
+    .eq("user_id", id);
+
+  if (error || !data) {
+    console.log(error);
+    return null;
+  }
+
+  return data as Tables<`post`>[];
 };
