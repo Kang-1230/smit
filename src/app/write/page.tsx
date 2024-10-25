efce5ca77584f0d646658e6dd6a116ffa9dd260f
hsw <hongsw6934@gmail.com>
2024-10-25 10:29:35 +0900

    feat : 스터디 생성 Form 작성,스터디 모집글 작성페이지 Form 작성

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,5 +1,55 @@
 "use client";
+import Link from "next/link";
+import React, { useState } from "react";
 
-export default function WritePage() {
-  return <div>스터디 모집글 작성 페이지입니다</div>;
-}
+export const Write = () => {
+  const [title, setTitle] = useState<string>("");
+  const [desription, setDiscription] = useState<string>("");
+  const [link, setLink] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+
+
+  const sendData = () => {
+    alert("모집글을 성공적으로 작성됨 -> 모집글로 페이지네이션");
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-between ... w-full p-5">
+        <Link href={"/"}>X</Link>
+        <p onClick={() => sendData()}>생성</p>
+      </div>
+      <h1>제목</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="제목을 작성해주세요."
+      />
+      <h1>{`시작 예정일 (UI 수정 예정)`}</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={desription}
+        onChange={(e) => setDiscription(e.target.value)}
+        placeholder="0000년 00월 00일"
+      />
+      <h1>{`스터디 그룹 선택 (UI 수정 예정)`}</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
+        placeholder="선택해주세요."
+      />
+      <h1>{`내용 (회의록 UI 수정)`}</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="내용을 작성해주세요"
+      />
+    </div>
+  );
+};
+
+export default Write;
--- src/app/write/study/page.tsx
+++ src/app/write/study/page.tsx
@@ -1,25 +1,125 @@
 "use client";
+import browserClient from "@/utils/supabase/client";
 import Image from "next/image";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
-export const page = () => {
+export const Study = () => {
   const params = useSearchParams();
-  const id = params.get("solo");
+  const isSolo: boolean = params.get("solo") === "true";
+  const [title, setTitle] = useState<string>("");
+  const [userCnt, setUserCnt] = useState<number>(1);
+  const [imgUrl, setImgUrl] = useState<string>();
+  const [studyCategory, setStudyCategory] = useState<string[]>([]);
+  const [studyTarget, setStudyTarget] = useState<string>();
+
+  // 기존 스터디 이미지 불러오기
+  const studyImg = browserClient.storage
+    .from("study_img")
+    .getPublicUrl("default").data.publicUrl;
+
+  // setImgUrl(studyImg);
+
+  const handleStudyCategory = (category: string) => {
+    setStudyCategory((prev) =>
+      prev.includes(category)
+        ? prev.filter((item) => item != category)
+        : [...prev, category],
+    );
+  };
+
+  const sendData = () => {
+    alert(
+      "모집글을 바로 작성하시겠어요? (모집글로 페이지네이션) 아닐 시 메인화면",
+    );
+  };
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div>
-        <h2>대표 이미지</h2>
-        <Image
-          src="/profile.png"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
+      <div className="flex justify-between ... w-full p-5">
+        <Link href={"/"}>X</Link>
+        <p onClick={() => sendData()}>생성</p>
       </div>
+      <h1>제목</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="제목을 작성해주세요."
+      />
+      <h1>한 줄 설명</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="그룹을 소개해주세요."
+      />
+      <h1>오픈채팅방 링크</h1>
+      <input
+        className="mb-1 border p-1 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="팀원들과 소통할 채팅방 링크를 넣어주세요."
+      />
+      <h1>인원 {`(UI 수정예정)`}</h1>
+      <div className="flex">
+        {userCnt !== 1 && !isSolo ? (
+          <button
+            className="bg-black size-8 text-white"
+            onClick={() => setUserCnt(userCnt - 1)}
+          >
+            -
+          </button>
+        ) : null}
+        <h1 className="text-2xl">{userCnt}</h1>
+        {!isSolo ? (
+          <button
+            className="bg-black size-8 text-white"
+            onClick={() => setUserCnt(userCnt + 1)}
+          >
+            +
+          </button>
+        ) : null}
+      </div>
+      <h1>직업 태그 {`(UI 수정예정)`}</h1>
+      <div className="flex justify-between ... bg-black text-white w-full">
+        <button onClick={() => setStudyTarget("대학생")}>대학생</button>
+        <button onClick={() => setStudyTarget("고등학생")}>고등학생</button>
+        <button onClick={() => setStudyTarget("중학생")}>중학생</button>
+        <button onClick={() => setStudyTarget("초등학생")}>초등학생</button>
+      </div>
+      <h1>{studyTarget}</h1>
+      <br></br>
+      <h1>스터디 태그 {`(UI 수정예정)`}</h1>
+      <div className="flex justify-between ... bg-black text-white w-full">
+        <button onClick={() => handleStudyCategory("수시모집")}>
+          수시모집
+        </button>
+        <button onClick={() => handleStudyCategory("UX/UI")}>UX/UI</button>
+        <button onClick={() => handleStudyCategory("개발")}>개발</button>
+        <button onClick={() => handleStudyCategory("중학생 입시")}>
+          중학생 입시
+        </button>
+        <button onClick={() => handleStudyCategory("고등학생 입시")}>
+          고등학생 입시
+        </button>
+        <button onClick={() => handleStudyCategory("미대 진학")}>
+          미대 진학
+        </button>
+        <button onClick={() => handleStudyCategory("체대 진학")}>
+          체대 진학
+        </button>
+        <button onClick={() => handleStudyCategory("일본어 JLPT N2")}>
+          일본어 JLPT N2
+        </button>
+        <button onClick={() => handleStudyCategory("취업 커리어")}>
+          취업 커리어
+        </button>
+      </div>
+      <h1>{studyCategory}</h1>
     </div>
   );
 };
 
-export default page;
+export default Study;
