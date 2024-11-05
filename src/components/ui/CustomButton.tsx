7841ee0c618f71107a17fbdaea4c3bf541ad4e4e
derek0k <derek0kim@gmail.com>
2024-11-05 17:42:48 +0900

    style: CustomButton

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -6,8 +6,12 @@ type Props = {
 export default function CustomButton({ text, onClick }: Props) {
   return (
     <button
-      className={`bg-[#1E1E1E] text-white py-[4px] px-[12px] rounded-[14px] text-[14px] font-semibold`}
-      style={{ lineHeight: "135%", letterSpacing: "-0.28px" }}
+      className={`bg-[#1E1E1E] text-white py-[4px] px-[12px] rounded-[14px] text-[14px] font-[00]`}
+      style={{
+        lineHeight: "135%",
+        letterSpacing: "-0.28px",
+        fontStyle: "normal",
+      }}
       onClick={onClick}
     >
       {text}
