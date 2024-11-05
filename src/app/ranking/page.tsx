cbf6b26872136b8db1805c014801d6c026895114
derek0k <derek0kim@gmail.com>
2024-11-06 05:51:43 +0900

    feat: 랭킹페이지 더보기 버튼 활성화

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,11 +1,43 @@
+"use client";
+
 import { fetchAllStudyByRanking } from "@/service/posts";
 import Image from "next/image";
 import Avatar from "./components/Avatar";
 import RankingCard from "./components/RankingCard";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useState } from "react";
 
-export default async function RankingPage() {
-  const ranking = await fetchAllStudyByRanking();
-  console.log(ranking);
+const fetchAllStudy = async (page: number) => {
+  const data = await fetchAllStudyByRanking(page);
+  return data;
+};
+
+export default function RankingPage() {
+  const [page, setPage] = useState(1);
+
+  const {
+    data: ranking,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ["ranking", page],
+    queryFn: () => fetchAllStudy(page),
+    placeholderData: (previousData) => previousData,
+  });
+
+  const handleMore = useCallback(() => {
+    setPage((prev) => prev + 1);
+    refetch();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error occurred while fetching data.</div>;
+  }
 
   return (
     <>
@@ -18,6 +50,7 @@ export default async function RankingPage() {
           width={375}
           height={375}
         />
+
         {/* 헤더 */}
         <div className="py-[64px] px-[24px] flex justify-between">
           <h1 className="relative z-10 text-[20px] leading-normal font-semibold">
@@ -29,20 +62,25 @@ export default async function RankingPage() {
         </div>
 
         {/* 아바타(1,2,3) */}
-        <div className="w-full absolute top-[125px] flex gap-[31px] justify-center">
-          <Avatar rank={2} study={ranking[1]} />
-          <Avatar rank={1} study={ranking[0]} />
-          <Avatar rank={3} study={ranking[2]} />
-        </div>
+        {ranking?.length && (
+          <div className="w-full absolute top-[125px] flex gap-[31px] justify-center">
+            <Avatar rank={2} study={ranking[1]} />
+            <Avatar rank={1} study={ranking[0]} />
+            <Avatar rank={3} study={ranking[2]} />
+          </div>
+        )}
 
+        {/* 랭킹카드들 */}
         <section className="bg-white w-full absolute top-[333px] rounded-t-20 px-[24px] py-[12px]">
-          {ranking.slice(3).map((study, i) => (
-            <RankingCard study={study} rank={i + 4} />
+          {ranking?.slice(3).map((study, i) => (
+            <RankingCard study={study} rank={i + 4} key={study.study_id} />
           ))}
           <div className="mb-24 mt-3 text-secondary-300 text-[14px] text-center">
-            <button>더보기</button>
+            <button onClick={handleMore}>더보기</button>
           </div>
         </section>
+
+        {/* 모달 */}
       </section>
     </>
   );
