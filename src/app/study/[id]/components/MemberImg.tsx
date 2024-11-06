cb8e7d88007697da936cdc21b4d07e193772b059
hsw <hongsw6934@gmail.com>
2024-11-07 04:00:37 +0900

    fix : build error fix

--- src/app/study/[id]/components/MemberImg.tsx
+++ src/app/study/[id]/components/MemberImg.tsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
 import { Tables } from "../../../../../database.types";
 import browserClient from "@/utils/supabase/client";
-import useModalOpen from "@/hooks/useModalOpen";
 
 const MemberImg = ({ user }: { user: Tables<"user"> }) => {
   const profileImg = browserClient.storage
     .from("profile_img")
     .getPublicUrl(user.profile_img ?? "default").data.publicUrl;
-
-
-  const { isModalOpen, modalOpen, modalClose } = useModalOpen();
 
   return (
     <div className="relative h-12 w-12">
@@ -18,7 +14,6 @@ const MemberImg = ({ user }: { user: Tables<"user"> }) => {
         alt={`${user.name}-img`}
         fill
         className="rounded-full border-[1px] border-white object-cover"
-        onClick={modalOpen}
       />
     </div>
   );
