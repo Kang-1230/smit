de6576d31bfceaeba252897bfb96582704d62be7
derek0k <derek0kim@gmail.com>
2024-11-06 17:11:38 +0900

    feat: 최근 스터디를 위한 PostCard로 변경

--- src/components/home/PostCard.tsx
+++ src/components/home/PostCard.tsx
@@ -4,72 +4,94 @@ import Link from "next/link";
 import LikeButton from "../common/LikeButton";
 import { PostWithRelations } from "@/service/posts";
 import OccupancyCounter from "./OccupancyCounter";
-
-const STYLES = {
-  card: {
-    white: "min-w-64 rounded-2xl bg-white p-4 shadow-md",
-    gray: "max-w-sm rounded-2xl bg-gray-50 p-4",
-  },
-  tag: {
-    white:
-      "rounded-full bg-gray-100 px-3 py-1 text-sm font-medium text-[#595959]",
-    gray: "rounded-full bg-white px-3 py-1 text-sm",
-  },
-};
+import Image from "next/image";
+import Badge from "../common/Badge";
 
 export type Props = {
   post: PostWithRelations;
-  variant?: "white" | "gray";
-  showAuthor?: boolean;
-  showStudyName?: boolean;
-  showLikesCount?: boolean;
 };
 
-export default function PostCard({
-  post,
-  variant = "white",
-  showAuthor = false,
-  showStudyName = false,
-  showLikesCount = false,
-}: Props) {
-  const { post_name, post_id, study_id, study, user } = post;
-  const { study_max_people, study_category, study_name } = study;
-  const { name } = user;
+export default function PostCard({ post }: Props) {
+  const { post_id, study_id, study, user } = post;
+  const {
+    study_max_people,
+    study_category,
+    study_name,
+    study_imgurl,
+    study_description,
+  } = study;
 
   return (
     <Link href={`/post/${post_id}`}>
-      <div className={STYLES.card[variant]}>
-        {showAuthor && (
-          <div className="mb-4 flex items-center gap-2">
-            <div className="h-6 w-6 rounded-full bg-gray-200"></div>
-            <span className="text-gray-600 text-sm">{name}</span>
+      <section className="flex h-[18rem] w-full flex-col justify-between gap-4 rounded-20 bg-white p-5">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-1 gap-2">
+            <div className="relative h-9 w-9 rounded-full border">
+              <Image
+                className="h-full w-full rounded-full object-cover"
+                src={study_imgurl || ""}
+                alt={study_name}
+                width={25}
+                height={25}
+              />
+            </div>
+            <div className="flex flex-col overflow-hidden font-normal text-secondary-700">
+              <div className="text-xs">{user.name}</div>
+              <div className="w-48 truncate text-sm font-normal">
+                {study_name}
+              </div>
+            </div>
           </div>
-        )}
-
-        {showStudyName && (
-          <div className="mb-2 text-base font-medium">{study_name}</div>
-        )}
-
-        <h2 className="mb-4 text-lg font-semibold">{post_name}</h2>
-
-        <div className="mb-4 flex flex-wrap gap-2">
-          {study_category.map((tag) => (
-            <span key={tag} className={STYLES.tag[variant]}>
-              {tag}
-            </span>
-          ))}
+          <LikeButton postId={post_id} isBoundary={false} />
         </div>
 
-        <div className="flex items-center justify-between text-sm text-gray-500">
-          <OccupancyCounter
-            studyId={study_id}
-            maxParticipants={study_max_people}
-          />
-          <div className="flex items-center gap-1">
-            <LikeButton postId={post_id} showLikesCount={showLikesCount} />
+        <div className="flex flex-1 flex-col gap-2">
+          <p className="line-clamp-3 w-full flex-wrap break-words font-semibold">
+            {study_description}
+          </p>
+          <div className="flex flex-wrap gap-1">
+            {study_category.map((category, idx) => (
+              <Badge
+                category={category}
+                key={`${study.study_id}-${category}`}
+                idx={idx}
+                color="tertiary"
+              />
+            ))}
           </div>
         </div>
-      </div>
+
+        <div className="flex justify-between border-t border-secondary-200 pt-4 text-sm">
+          <div>
+            <OccupancyCounter
+              studyId={study_id}
+              maxParticipants={study_max_people}
+            />
+          </div>
+          <div className="flex gap-2 text-sm font-normal text-secondary-400">
+            <div className="flex gap-1">
+              <Image
+                className="h-full w-full"
+                src="/icons/HeartGray.svg"
+                alt="heart"
+                width={15}
+                height={15}
+              />
+              14
+            </div>
+            <div className="flex gap-[0.1rem]">
+              <Image
+                className="h-full w-full"
+                src="/icons/CommentGray.svg"
+                alt="comment"
+                width={15}
+                height={15}
+              />
+              8
+            </div>
+          </div>
+        </div>
+      </section>
     </Link>
   );
 }
