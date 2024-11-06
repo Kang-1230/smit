0accbfcef2799ea27ca91714c3cafa969a55ed24
derek0k <derek0kim@gmail.com>
2024-11-06 17:13:11 +0900

    style: MainPage 스타일 수정

--- src/components/home/FilterablePosts.tsx
+++ src/components/home/FilterablePosts.tsx
@@ -23,30 +23,23 @@ export default function FilterablePosts() {
   if (!posts) return <>Posts가 없습니다</>;
 
   return (
-    <section className="my-4 flex flex-col gap-4 px-6">
-      <h2 className="text-xl font-medium flex gap-1">
-        <Image src={`/icons/Book.svg`} width={20} height={20} alt="user" />
+    <section className="mt-4 flex flex-col bg-[#F6F6F4] px-6 pb-20 pt-10">
+      <h2 className="flex gap-1 text-xl font-medium">
+        <Image src={`/icons/Book.svg`} width={23} height={23} alt="user" />
         최근 스터디
       </h2>
-      <p className="text-secondary-500 text-sm">
-        최근 가장 많은 관심을 받았어요.
-      </p>
+
       <Category
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}
         selectedJobs={selectedJobs}
         setSelectedJobs={setSelectedJobs}
       />
-      <ul className="flex flex-col gap-2">
+
+      <ul className="flex flex-col gap-5">
         {posts?.map((post) => (
           <li key={post.post_id}>
-            <PostCard
-              post={post}
-              variant="gray"
-              showAuthor={true}
-              showStudyName={true}
-              showLikesCount={true}
-            />
+            <PostCard post={post} />
           </li>
         ))}
       </ul>
