faf93e266f615e63c522aa8416932972b585e635
hadooni <hayoung7259@gmail.com>
2024-11-06 17:44:55 +0900

    design: 디자인 시안 css 적용

--- src/app/study/[id]/[date]/page.tsx
+++ src/app/study/[id]/[date]/page.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns-tz";
 import { ko } from "date-fns/locale";
 import EventList from "../components/EventList";
 import { fetchStudyInfo } from "@/utils/supabase/supabase-server";
-import BackButton from "../components/BackButton";
+import BackButton from "../../../../components/common/BackButton";
 
 interface Props {
   params: {
@@ -28,7 +28,7 @@ const CalendarPage = async ({ params }: Props) => {
     <div className="bg-[#F6F6F4] h-full w-full">
       <header className="mx-6">
         <div className="flex justify-center items-center relative pt-[10px]">
-          <BackButton />
+          <BackButton color="#666666" />
           <span className="body-16-s text-secondary-600">일정잡기</span>
         </div>
         <div className="flex items-center mt-[30px]">
