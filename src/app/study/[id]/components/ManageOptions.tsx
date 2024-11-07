362d206997b9d7977eecaf3c40770cf13626f37e
hadooni <hayoung7259@gmail.com>
2024-11-07 12:06:36 +0900

    fix: 빌드 오류 해결

--- src/app/study/[id]/components/ManageOptions.tsx
+++ src/app/study/[id]/components/ManageOptions.tsx
@@ -8,11 +8,13 @@ const ManageOptions = () => {
   return (
     <div className="body-16-m mb-[52px] flex justify-center border-b border-secondary-300 text-secondary-300">
       <button
+        onClick={() => setIsSelected(true)}
         className={`flex flex-1 justify-center p-2 ${isSelected ? "" : SelectStyle}`}
       >
         스터디원 관리
       </button>
       <button
+        onClick={() => setIsSelected(true)}
         className={`flex flex-1 justify-center p-2 ${isSelected ? SelectStyle : ""}`}
       >
         스터디 편집
