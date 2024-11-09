415cb0201ace120237e918d92db1281779f11755
derek0k <derek0kim@gmail.com>
2024-11-09 17:10:59 +0900

    feat: LoadingPage 구현

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,32 @@
+export default function Loading() {
+  const text = "LOADING...";
+  const textArr = text.split("");
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-9">
+      {/* 시계 */}
+      <div className="clock absolute top-[40%] h-[52px] w-[52px] rotate-45">
+        {Array.from({ length: 12 }, (_, index) => (
+          <span
+            className="absolute h-full w-full"
+            key={index}
+            style={{ "--i": index + 1 } as React.CSSProperties}
+          />
+        ))}
+      </div>
+
+      {/* 시침 */}
+      <div className="hand absolute top-[40%]"></div>
+
+      {/* 문구 */}
+      <div
+        className="animate-loading absolute top-[40%] flex gap-[0.05rem] pt-[6.25rem] font-pretendard text-sm font-normal text-secondary-200"
+        style={{ lineHeight: "135%", fontStyle: "normal" }}
+      >
+        {textArr.map((item, idx) => (
+          <span className={`animate-loading delay-${idx * 100}`}>{item}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
