38de4dfc39fa1d1292d6a16d087f6154c4f0fcf1
doo1b <tngml928@gmail.com>
2024-11-14 19:46:39 +0900

    design: 작성한 글 제목 길면 없애기

--- src/app/mypage/components/MyPostCard.tsx
+++ src/app/mypage/components/MyPostCard.tsx
@@ -31,7 +31,9 @@ const MyPostCard = ({
   return (
     <div>
       <div className="flex h-10 flex-row items-center justify-between">
-        <p className="body-16-m">{post.post_name}</p>
+        <p className="body-16-m min-w-0 max-w-full flex-1 overflow-hidden text-ellipsis text-nowrap pr-[3px]">
+          {post.post_name}
+        </p>
         <div className="flex flex-row gap-x-1">
           {/* 수정 페이지로 link 필요 */}
           <Link href={`/write?post=${post.post_id}`} key={post.post_id}>
