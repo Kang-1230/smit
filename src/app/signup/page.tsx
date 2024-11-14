91f26ad8cb6c0ce06c8419caa2ae3dfa2b572be3
KangSujin <tnwls7138@gmail.com>
2024-11-14 09:54:05 +0900

    fix : 필요없는 파일 삭제

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -58,26 +58,18 @@ export default function SignupPage() {
 
     if (existingUser) {
       alert("이미 존재하는 닉네임입니다.");
-      return;
+      return false;
     }
+
+    alert("사용 가능한 닉네임입니다.");
+    return true;
   };
 
   const onSubmit = async (formData: SignUpFormData) => {
     if (formData.verifiedPassword) {
       if (formData.verifiedPassword !== formData.password) {
-        return errors.password?.message;
+        return <p>{errors.password?.message}</p>;
       }
-    }
-
-    const { data: existingUser } = await supabase
-      .from("user")
-      .select("name")
-      .eq("name", formData.nickname)
-      .single();
-
-    if (existingUser) {
-      alert("이미 존재하는 닉네임입니다.");
-      return;
     }
 
     // 모든 필드 검증
