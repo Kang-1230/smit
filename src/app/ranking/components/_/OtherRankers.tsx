ed98ad8f93abced4b316a986cc99c89033ef5e77
derek0k <derek0kim@gmail.com>
2024-11-15 16:46:00 +0900

    fix: 랭킹 페이지에 무한 스크롤 적용 및 로딩 화면 디자인 시안 반영

--- src/app/ranking/components/_/OtherRankers.tsx
+++ src/app/ranking/components/_/OtherRankers.tsx
@@ -1,5 +1,7 @@
 import { Study } from "@/types/studys";
 import RankingCard from "./RankingCard";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 type Props = {
   rankers: Study[];
@@ -7,17 +9,26 @@ type Props = {
   hasNextPage: boolean;
   isFetchingNextPage: boolean;
   handleModalClick: (id: string) => void;
+  fetchNextPage: () => void;
 };
 
 export default function OtherRankers({
   rankers,
-  loadMoreData,
   hasNextPage,
+  fetchNextPage,
   isFetchingNextPage,
   handleModalClick,
 }: Props) {
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage, isFetchingNextPage]);
+
   return (
-    <section className="absolute top-[333px] h-full w-full rounded-t-20 bg-gradient-to-b from-[#FFFCF9] via-[#FFF] to-[#FFF] px-[24px] py-[12px] backdrop-blur-[15px]">
+    <section className="absolute top-[333px] w-full rounded-t-20 bg-gradient-to-b from-[#FFFCF9] via-[#FFF] to-[#FFF] px-[24px] py-[12px] backdrop-blur-[15px]">
       {rankers.map((ranker, index) => (
         <RankingCard
           study={ranker}
@@ -25,17 +36,11 @@ export default function OtherRankers({
           handleModalClick={handleModalClick}
         />
       ))}
-      <div className="mb-24 mt-3 text-center text-[14px] text-secondary-300">
-        <button
-          onClick={loadMoreData}
-          disabled={!hasNextPage || isFetchingNextPage}
-        >
-          {isFetchingNextPage
-            ? "로딩 중..."
-            : hasNextPage
-              ? "더보기"
-              : "마지막 항목입니다"}
-        </button>
+      <div
+        className="mb-24 mt-3 text-center text-[14px] text-secondary-300"
+        ref={ref}
+      >
+        {isFetchingNextPage ? "로딩 중..." : "마지막 항목입니다"}
       </div>
     </section>
   );
--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,11 +1,9 @@
 import FloatingButtons from "@/components/common/FloatingButtons";
-import RankingHeader from "./components/RankingHeader";
 import RankingList from "./components/_/RankingList";
 
-export default function Page() {
+export default function RankingPage() {
   return (
-    <section className="relative h-screen bg-gradient-to-b from-[#FF8F32] to-[#FFC799]">
-      <RankingHeader />
+    <section>
       <RankingList />
       <FloatingButtons />
     </section>
