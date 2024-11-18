f652c3a087142cda662cd9f83f55ca9867985f4c
hsw <hongsw6934@gmail.com>
2024-11-18 17:03:18 +0900

    fix : build error fix

--- src/app/write/components/WriteModal.tsx
+++ src/app/write/components/WriteModal.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { Tables } from "../../../../database.types";
 import Image from "next/image";
