c8e890cbd863380b3f98a13cd0dc2eaa461dd8cd
hsw <hongsw6934@gmail.com>
2024-11-18 09:46:44 +0900

    design : RoundSelectDiv 폰트 색상 변경

--- src/app/write/components/RoundSelectDiv.tsx
+++ src/app/write/components/RoundSelectDiv.tsx
@@ -16,7 +16,7 @@ const RoundSelectDiv = ({
       <div className="flex w-full justify-between">
         <p className="text-secondary-700">{title}</p>
         <div className="flex items-center">
-          <p className="body-16-m text-secondary-300" onClick={onClick}>
+          <p className="body-16-m text-secondary-500" onClick={onClick}>
             {value}
           </p>
           <Image
