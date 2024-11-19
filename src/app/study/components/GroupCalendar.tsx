cd6d0f6a39a97ac0779f6209d429ff4f837da579
hadooni <hayoung7259@gmail.com>
2024-11-19 09:47:21 +0900

    design: 그룹페이지 웹 반응형 적용

--- src/app/study/components/GroupCalendar.tsx
+++ src/app/study/components/GroupCalendar.tsx
@@ -30,15 +30,21 @@ const GroupCalendar = ({ studyId }: { studyId: string }) => {
 
   return (
     <div
-      className="relative my-6 h-[362px] w-[327px] rounded-[20px] border border-[#797272]"
+      className="relative my-6 h-[362px] w-[327px] rounded-[20px] border border-[#797272] md:h-[474px] md:w-[408px]"
       style={{
         background:
           "radial-gradient(64.61% 66.48% at 20.51% 9.53%, rgba(255, 153, 69, 0.11) 20%, rgba(255, 153, 69, 0.00) 100%), linear-gradient(180deg, rgba(47, 47, 47, 0.50) 0%, rgba(103, 103, 103, 0.30) 100%)",
       }}
     >
-      <div className="relative ml-4 mt-4 flex items-center gap-1">
-        <Image src={CalendarMonth} alt="calendar" width={16} height={16} />
-        <span className="caption font-medium text-secondary-300">
+      <div className="relative ml-4 mt-4 flex items-center gap-1 md:ml-5 md:mt-6">
+        <Image
+          src={CalendarMonth}
+          alt="calendar"
+          width={16}
+          height={16}
+          className="h-4 w-4 md:h-5 md:w-5"
+        />
+        <span className="caption md:body-14-r font-medium text-secondary-300">
           일정 잡기
         </span>
         {tooltipVisible && (
@@ -68,8 +74,10 @@ const GroupCalendar = ({ studyId }: { studyId: string }) => {
           }}
           className="flex w-full items-center justify-center rounded-md"
           classNames={{
-            nav_button_previous: "absolute left-[85px] top-[3px] !w-3 !h-3",
-            nav_button_next: "absolute right-[85px] top-[3px] !w-3 !h-3",
+            nav_button_previous:
+              "absolute left-[85px] top-[3px] !w-3 !h-3 md:left-[122px]",
+            nav_button_next:
+              "absolute right-[85px] top-[3px] !w-3 !h-3 md:right-[122px]",
             nav_button: "!p-0 bg-transparent opacity-50 hover:opacity-50",
           }}
           modifiers={{ hasEvent: eventDates }}
