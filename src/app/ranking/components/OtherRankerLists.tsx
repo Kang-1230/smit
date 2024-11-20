716f345fc00bf2948c5ce4993e314aee3119ca67
derek0k <derek0kim@gmail.com>
2024-11-21 04:46:56 +0900

    feat: 랭킹 페이지 반응형 구현 완료

--- src/app/ranking/components/OtherRankerLists.tsx
+++ src/app/ranking/components/OtherRankerLists.tsx
@@ -1,7 +1,7 @@
 import { Study } from "@/types/studys";
-import RankingCard from "./RankingCard";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
+import RankingCard from "./RankingCard";
 
 type Props = {
   rankers: Study[];
@@ -12,7 +12,7 @@ type Props = {
   fetchNextPage: () => void;
 };
 
-export default function OtherRankers({
+export default function OtherRankersList({
   rankers,
   hasNextPage,
   fetchNextPage,
@@ -28,14 +28,17 @@ export default function OtherRankers({
   }, [inView, fetchNextPage, hasNextPage, isFetchingNextPage]);
 
   return (
-    <section className="absolute top-[333px] w-full rounded-t-20 bg-gradient-to-b from-[#FFFCF9] via-[#FFF] to-[#FFF] px-[24px] py-[12px] backdrop-blur-[15px]">
-      {rankers.map((ranker, index) => (
-        <RankingCard
-          study={ranker}
-          key={ranker.id + index}
-          handleModalClick={handleModalClick}
-        />
-      ))}
+    <section className="relative mx-0 max-w-full rounded-t-[37px] bg-white px-6 py-3 shadow-2xl md:px-5 md:py-5 lg:mx-6 xl:mx-0">
+      <div className="absolute left-0 top-0 z-0 h-56 w-full rounded-t-[37px] bg-gradient-to-b from-[#FFF3E7] to-[#FFF]"></div>
+      <div className="relative z-0">
+        {rankers.map((ranker, index) => (
+          <RankingCard
+            study={ranker}
+            key={ranker.id + index}
+            handleModalClick={handleModalClick}
+          />
+        ))}
+      </div>
       <div
         className="mb-24 mt-3 text-center text-[14px] text-secondary-300"
         ref={ref}
