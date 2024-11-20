a1d4b911e6a997a7365469ceab6ac1c1b64130fe
doo1b <tngml928@gmail.com>
2024-11-20 21:39:07 +0900

    fix: lighthouse 최적화

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,19 @@ const nextConfig = {
     ],
     formats: ["image/avif", "image/webp"],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // 모든 경로에 Cache-Control 헤더를 적용
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "private, max-age=0, must-revalidate", // 원하는 Cache-Control 값 설정
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
