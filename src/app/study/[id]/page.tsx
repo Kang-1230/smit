79cccec1239efcbbd8b6b1b5be69490645491e27
doo1b <tngml928@gmail.com>
2024-11-20 12:41:16 +0900

    design: 그룹페이지 배경색 수정

--- src/app/study/[id]/page.tsx
+++ src/app/study/[id]/page.tsx
@@ -20,23 +20,25 @@ const Page = async ({ params }: { params: { id: string } }) => {
   const studyMember = await fetchStudyMember(studyId);
 
   return (
-    <div className="w-full xl:mx-auto xl:max-w-[1280px]">
-      <BackButton className="ml-6 mt-[10px]" />
-      <div className="flex w-full flex-col items-center overflow-x-hidden bg-secondary-800 px-6 pt-[64px] text-white">
-        <StudyInfo study={study} member={studyMember} />
-        <main className="w-full xl:grid xl:grid-rows-[402px_474px] xl:gap-y-[24px]">
-          <StudyStateBox
-            studyId={studyId}
-            member={studyMember}
-            today={today}
-            study={study}
-          />
-          <section className="xl:grid xl:grid-cols-3 xl:gap-x-6">
-            <div className="hidden xl:block">Daily Planner</div>
-            <GroupCalendar studyId={studyId} />
-            <PersonalMemos studyId={studyId} />
-          </section>
-        </main>
+    <div className="h-full w-full bg-secondary-800">
+      <div className="w-full xl:mx-auto xl:max-w-[1280px]">
+        <BackButton className="ml-6 mt-[10px]" />
+        <div className="flex w-full flex-col items-center overflow-x-hidden bg-secondary-800 px-6 pt-[64px] text-white">
+          <StudyInfo study={study} member={studyMember} />
+          <main className="w-full xl:grid xl:grid-rows-[402px_474px] xl:gap-y-[24px]">
+            <StudyStateBox
+              studyId={studyId}
+              member={studyMember}
+              today={today}
+              study={study}
+            />
+            <section className="xl:grid xl:grid-cols-3 xl:gap-x-6">
+              <div className="hidden xl:block">Daily Planner</div>
+              <GroupCalendar studyId={studyId} />
+              <PersonalMemos studyId={studyId} />
+            </section>
+          </main>
+        </div>
       </div>
     </div>
   );
