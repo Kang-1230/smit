c717140bfb0c0ee0574e67571fdaf2e8c657e489
hsw <hongsw6934@gmail.com>
2024-11-20 10:13:48 +0900

    feat : ScrollPicker 현재 날짜로 변경되도록 수정 반영

--- src/components/common/ScrollPicker.tsx
+++ src/components/common/ScrollPicker.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 interface ScrollPickerProps {
   options: string[];
   handleScroll: (e: React.UIEvent<HTMLDivElement>) => void;
-  selectedItem: string;
+  selectedItem: string | number | undefined;
 }
 
 const ScrollPicker = ({
@@ -11,28 +13,47 @@ const ScrollPicker = ({
   handleScroll,
   selectedItem,
 }: ScrollPickerProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const executionCount = useRef(0); // useRef를 사용하여 실행 횟수 추적
+
+  useEffect(() => {
+    // 실행 횟수가 5번 미만일 때만 실행
+    if (executionCount.current < 5) {
+      if (containerRef.current && selectedItem !== undefined) {
+        const selectedIndex = options.indexOf(selectedItem.toString());
+        if (selectedIndex !== -1) {
+          // 스크롤을 selectedItem에 맞게 설정
+          containerRef.current.scrollTop = selectedIndex * 40;
+        }
+      }
+
+      // 실행 횟수 증가
+      executionCount.current += 1;
+    }
+  }, [selectedItem, options]); // selectedItem, options가 변경될 때만 실행
+
   return (
-    <div className="h-[140px] relative w-16">
+    <div className="relative h-[140px] w-16">
       <div
-        className={`absolute pointer-events-none  ${
+        className={`pointer-events-none absolute ${
           options.length > 0 && options[0].length < 5
-            ? "w-10 top-[50px] right-[14px]"
-            : "w-[50px] top-[50px] right-[8.5px]"
-        }  h-10 w-10 border-y-2`}
+            ? "right-[14px] top-[50px] w-10"
+            : "right-[8.5px] top-[50px] w-[50px]"
+        } h-10 w-10 border-y-2`}
       />
       <div
-        className="h-full overflow-auto scrollbar-hide snap-y snap-mandatory overscroll-contain py-[60px]"
+        className="scrollbar-hide h-full snap-y snap-mandatory overflow-auto overscroll-contain py-[60px]"
         onScroll={handleScroll}
+        ref={containerRef} // ref 연결
       >
         {options.map((item) => (
           <div
             key={item}
-            className={`h-[40px] flex items-center justify-center snap-center
-        ${
-          selectedItem === item
-            ? "text-black font-medium mt:border-solid"
-            : "text-gray-400"
-        }`}
+            className={`flex h-[40px] snap-center items-center justify-center ${
+              selectedItem === item
+                ? "mt:border-solid font-medium text-black"
+                : "text-gray-400"
+            }`}
           >
             {item}
           </div>
